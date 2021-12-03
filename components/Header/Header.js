c029cadbe9a176350be1049683fc27321db62ad8
Adewole Adewumi <wolejr@gmail.com>
2021-12-02 22:34:19 -0500

    Added mobile menu

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -9,6 +9,15 @@ export default function Header() {
                     <a href="#about">About</a>
                     <a href="#contact">Contact</a>
                 </div>
+                <div className="header__mobile-menu">
+                    <i className="fas fa-bars"></i>
+                    <ul className="header__mobile-links">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#feature">Features</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
             </div>
         </header>
     )
