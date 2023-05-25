3969632e5f5f1098217c8900f066a4e8e6f6cf71
Adewole Adewumi <wolejr@gmail.com>
2023-05-25 02:20:13 -0400

    Modified features page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import MainLayout from '../components/layouts/MainLayout'
 import Hero from '../components/Hero/Hero'
-import Features from '../components/Features/Features'
+import Features from "../components/Features/Features"
 import About from '../components/About/About'
 import Contact from '../components/Contact/Contact'
 
